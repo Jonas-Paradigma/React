8fa8de66bf9f9554a496f3ec41346e60ab3cf503
Jonas-Paradigma <jonas.nagel@student.htldornbirn.at>
2025-01-10 09:35:42 +0100

    React und State

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/Button'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-      <h1>jjj</h1>
-      
+    <div className="App">
+      <header className="App-header">
+        <Button name="Hans" age="17"/>
+
+        hallo
+      </header>
+    </div>
   )
 }
 
