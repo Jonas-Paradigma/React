c66762b1ad974e67247ea6504b10a78dc7a4c11b
Jonas-Paradigma <jonas.nagel@student.htldornbirn.at>
2025-01-15 13:10:52 +0100

    Tailwind

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -1,18 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import Button from './components/Button'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button name="Hans" age="17"/>
-
-        hallo
-      </header>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
 
