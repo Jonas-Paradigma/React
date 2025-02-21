6b2f7b504e79cd3e78aad2df1c9ac08a6083ed44
Jonas-Paradigma <jonas.nagel@student.htldornbirn.at>
2025-02-21 09:28:03 +0100

    Topbar stuktur

--- Movie_react_API/Movie_react_API/src/App.jsx
+++ Movie_react_API/Movie_react_API/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
-import MovieDetailsPage from "./pages/MovieDetails";
+import MovieDetails from "./pages/MovieDetails"; // FIX: Name korrigiert
 
 const App = () => {
   return (
@@ -10,10 +10,13 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
-        <Route path="/moviename" element={<MovieDetailsPage />} />
+        <Route path="/moviename" element={<MovieDetails />} />
       </Routes>
     </Router>
+
+    
   );
+  
 };
 
 export default App;
